ff6e68b66fc2bb30899428958443cdee13eacb03
beefoo <brian@youaremyjoy.org>
2024-03-18 21:47:14 -0400

    Add rainbow tool

--- godot/main.gd
+++ godot/main.gd
@@ -10,7 +10,8 @@ var tool_config = {
 		{"name": "Pixelate", "icon": "art/icons/copy.svg", "shader_params": []},
 		{"name": "Kaleidoscope", "icon": "art/icons/shuffle.svg", "shader_params": ["pointer"]},
 		#{"name": "Distort", "icon": "art/icons/copy.svg", "shader_params": ["pointer_start", "pointer"]},
-		{"name": "Waves", "icon": "art/icons/copy.svg", "shader_params": []},	
+		#{"name": "Waves", "icon": "art/icons/copy.svg", "shader_params": []},
+		{"name": "Rainbow", "icon": "art/icons/copy.svg", "shader_params": ["pointer"]},
 	]
 }
 
