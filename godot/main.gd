c56d299f7f6519562d8abe4f7bc8f563222fc2cf
beefoo <brian@youaremyjoy.org>
2024-03-23 09:38:20 -0400

    Add bend tool

--- godot/main.gd
+++ godot/main.gd
@@ -21,6 +21,7 @@ var tool_config = {
 		{"name": "Weave", "icon": "art/icons/shuffle.svg", "shader_params": []},
 		{"name": "Drain", "icon": "art/icons/copy.svg", "shader_params": []},
 		{"name": "Reflect", "icon": "art/icons/shuffle.svg", "shader_params": []},
+		{"name": "Bend", "icon": "art/icons/copy.svg", "shader_params": ["aspect_ratio", "pointer"]},
 	]
 }
 
