f3647e901d1a0212c4f7692c82a79e27a6214873
beefoo <brian@youaremyjoy.org>
2024-04-14 22:51:25 -0400

    Add script to download metadata

---
+++ scripts/get_metadata.py
@@ -0,0 +1,52 @@
+import argparse
+import time
+
+import pandas as pd
+from tqdm import tqdm
+
+from helper import get_api_data
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", dest="DATA_FILE", default="data/collection.csv", help="Path to .csv data file")
+    parser.add_argument("--cache", dest="CACHE_DIR", default="tmp/", help="Cache API responses here")
+    parser.add_argument("--debug", dest="DEBUG", action="store_true", help="Only print details; do not update file")
+    return parser.parse_args()
+
+def main(args):
+    df = pd.read_csv(args.DATA_FILE, dtype=str)
+    print(f"Read {df.shape[0]:,} rows from {args.DATA_FILE}")
+
+    fields_to_check = ["Title", "Creator", "Date", "ImageURL"]
+    for i, row in tqdm(df.iterrows()):
+        needs_update = False
+        for field in fields_to_check:
+            if pd.isnull(row[field]):
+                needs_update = True
+                break
+        
+        if not needs_update:
+            continue
+
+        api_data = get_api_data(row["SourceId"], row["ItemId"], args.CACHE_DIR, args.DEBUG)
+        time.sleep(1.0)
+
+        if "error" in api_data:
+            print(api_data["error"])
+            continue
+        
+        if args.DEBUG:
+            print(f"Updating {row['Id']}")
+
+        for field in fields_to_check:
+            if pd.isnull(row[field]) and field in api_data:
+                df.at[i, field] = api_data[field]
+                if args.DEBUG:
+                    print(f"  {field} => {api_data[field]}")
+    
+    if not args.DEBUG:
+        df.to_csv(args.DATA_FILE)
+    # df.to_csv(args.DATA_FILE)
+    print("Done.")
+
+main(parse_args())
---
+++ scripts/helper.py
@@ -0,0 +1,142 @@
+import json
+import os
+
+import requests
+
+def get_api_data(source_id, item_id, cache_dir=None, verbose=False):
+    """Retrieve normalized API data from a data source"""
+    resp = {}
+    if source_id == "clevelandart":
+        resp = get_api_data_clevelandart(item_id, cache_dir, verbose)
+    elif source_id == "metmuseum":
+        resp = get_api_data_metmuseum(item_id, cache_dir, verbose)
+    elif source_id == "si":
+        resp = get_api_data_si(item_id, cache_dir, verbose)
+    else:
+        resp["error"] = f"No API service defined for {source_id}"
+    return resp
+
+def get_api_data_clevelandart(item_id, cache_dir=None, verbose=False):
+    """Retrieve normalized API data from a Cleveland Art Museum API"""
+    resp = {}
+    api_url = f"https://openaccess-api.clevelandart.org/api/artworks/{item_id}"
+    cache_file = f"{cache_dir}clevelandart_{item_id}.json" if cache_dir is not None else None
+    api_resp = get_json(api_url, cache_file, verbose)
+    if "data" not in api_resp or "id" not in api_resp["data"]:
+        error = api_resp["error"] if "error" in api_resp else "Unknown error"
+        resp["error"] = f"Could not find object from: {api_url} ({error})"
+    else:
+        if cache_file:
+            write_json(cache_file, api_resp)
+        data = api_resp["data"]
+        resp["Title"] = get_nested_value(data, "title", "Untitled")
+        resp["Creator"] = get_nested_value(data, ["creators", 0, "description"], "Unknown")
+        resp["Date"] = get_nested_value(data, "creation_date", "Unknown")
+        resp["ImageURL"] = get_nested_value(data, ["images", "print", "url"], "")
+    return resp
+
+def get_api_data_metmuseum(item_id, cache_dir=None, verbose=False):
+    """Retrieve normalized API data from a Met Museum API"""
+    resp = {}
+    api_url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{item_id}"
+    cache_file = f"{cache_dir}metmuseum_{item_id}.json" if cache_dir is not None else None
+    api_resp = get_json(api_url, cache_file, verbose)
+    if "objectID" not in api_resp:
+        error = api_resp["error"] if "error" in api_resp else "Unknown error"
+        resp["error"] = f"Could not find object from: {api_url} ({error})"
+    else:
+        if cache_file:
+            write_json(cache_file, api_resp)
+        resp["Title"] = get_nested_value(api_resp, "title", "Untitled")
+        resp["Creator"] = get_nested_value(api_resp, "artistDisplayName", "Unknown")
+        resp["Date"] = get_nested_value(api_resp, "objectDate", "Unknown")
+        resp["ImageURL"] = get_nested_value(api_resp, "primaryImage", "")
+    return resp
+
+def get_api_data_si(item_id, cache_dir=None, verbose=False):
+    """Retrieve normalized API data from a Smithsonian Institute API"""
+    resp = {}
+    api_keys = read_json("api_keys.json")
+    if "si" not in api_keys:
+        resp["error"] = "You must set Smithsonian API key in api_keys.json with key 'si"
+        return resp
+    api_key = api_keys["si"]
+    api_url = f"https://api.si.edu/openaccess/api/v1.0/content/edanmdm:{item_id}?api_key={api_key}"
+    cache_file = f"{cache_dir}si_{item_id}.json" if cache_dir is not None else None
+    api_resp = get_json(api_url, cache_file, verbose)
+    if "status" not in api_resp or api_resp["status"] != 200:
+        error = api_resp["error"] if "error" in api_resp else "Unknown error"
+        resp["error"] = f"Could not find object from: {api_url} ({error})"
+    else:
+        if cache_file:
+            write_json(cache_file, api_resp)
+        data = api_resp["response"]
+        resp["Title"] = get_nested_value(data, "title", "Untitled")
+        resp["Creator"] = get_nested_value(data, ["content", "indexedStructured", "name", 0], "Unknown")
+        resp["Date"] = get_nested_value(data, ["content", "freetext", "date", 0, "content"], "Unknown")
+        resp["ImageURL"] = get_nested_value(data, ["content", "descriptiveNonRepeating", "online_media", "media", 0, "content"], "")
+    return resp
+
+def get_json(url, cache_file=None, verbose=False):
+    """Parse JSON from a URL"""
+
+    if cache_file and os.path.isfile(cache_file):
+        if verbose:
+            print(f"Data already requested for {url}. Loading from cache.")
+        return read_json(cache_file)
+
+    if verbose:
+        print(f"Requesting data from {url}...")
+
+    data = {}
+    try:
+        response = requests.get(url, timeout=30)
+        data = response.json()
+    except requests.HTTPError:
+        data = {"error": "HTTPError"}
+    except requests.Timeout:
+        data = {"error": "Timeout"}
+    except requests.JSONDecodeError:
+        data = {"error": "JSONDecodeError"}
+
+    return data
+
+def get_nested_value(root, nodes, default_value=""):
+    """Get a value from a nested dict"""
+    value = default_value
+    found = True
+    if not isinstance(nodes, list):
+        nodes = [nodes]
+
+    for node in nodes:
+        if isinstance(node, int):
+            if isinstance(root, list) and len(root) > node:
+                root = root[node]
+            else:
+                found = False
+                break
+        elif isinstance(root, dict) and node in root:
+            root = root[node]
+        else:
+            found = False
+            break
+
+    if found:
+        value = root
+        if value == "":
+            value = default_value
+
+    return value
+
+def read_json(filename):
+    """Read a JSON file"""
+    data = {}
+    with open(filename) as f:
+        data = json.load(f)
+    return data
+
+
+def write_json(filename, data):
+    """Write data to JSON file"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
