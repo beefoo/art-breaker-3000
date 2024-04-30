418e1365bc5b139540760cf2da4a84842739e8f1
beefoo <brian@youaremyjoy.org>
2024-04-30 17:18:03 -0400

    Make modals responsive

--- godot/ItemDetail.gd
+++ godot/ItemDetail.gd
@@ -9,12 +9,12 @@ func _ready():
 	title_template += "[font_size=60][color=#dddddd]{Creator}, {Date}[/color][/font_size]\n"
 	title_template += "[url={URL}][font_size=60][color=#f7de38]{Source}[/color][/font_size][/url]"
 	
-	$TextContainer/CloseButton.pressed.connect(close)
-	$TextContainer/Text.meta_clicked.connect(_on_link_clicked)
+	$HBox/TextContainer/CloseButton.pressed.connect(close)
+	$HBox/TextContainer/Text.meta_clicked.connect(_on_link_clicked)
 
 func _on_link_clicked(url):
 	OS.shell_open(url)
 
 func set_item(texture, data):
-	$ImageContainer/TextureRect.set_texture(texture.duplicate())
-	$TextContainer/Text.set_text(title_template.format(data))
+	$HBox/ImageContainer/TextureRect.set_texture(texture.duplicate())
+	$HBox/TextContainer/Text.set_text(title_template.format(data))
