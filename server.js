5e7b7297bda069fddf152ddc18eb90f3958d8616
beefoo <brian@youaremyjoy.org>
2024-09-15 21:36:04 -0400

    enable threads for web export and change samples to streams for web audio

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./html/')); //Tells the app to serve static files from ./
+app.use(express.static('./html/', {
+    setHeaders: function(res) {
+        res.set('Cross-Origin-Opener-Policy', 'same-origin');
+        res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+    }
+})); //Tells the app to serve static files from ./
 
 app.listen(port, () => console.log('Listening on port '+port));
